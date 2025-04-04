9a492a3305f4a8870a69e9d08099ead95b111fd9
divyanshsingh <divyansh@spinoinc.com>
2025-04-04 17:29:29 +0530

    Base url changed to dev url

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,7 +1,6 @@
 const environment = {
-  nodeApiUrl: process.env.REACT_APP_NODE_API_URL, // http://localhost:3001
-  dotNetApiUrl: process.env.REACT_APP_DOT_NET_API_URL, // http://localhost:3002
- 
+  nodeApiUrl: process.env.REACT_APP_NODE_API_URL,
+  dotNetApiUrl: process.env.REACT_APP_ETOKEN_API_URL,
 };
 
 export default environment;
