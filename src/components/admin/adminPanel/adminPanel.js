8a858869131f19433a4e8a170f1af971502a1cc7
Ova2 Consultants <hr@ova2consultancy.com>
2025-05-28 02:25:42 -0400

    .

--- src/components/admin/adminPanel/adminPanel.js
+++ src/components/admin/adminPanel/adminPanel.js
@@ -1,16 +1,16 @@
 import React from "react";
 import TileContainer from "../../common/tileContainer/tileContainer";
-import { jobTilesData, ova2EtokenMainTile } from "../../../constants/tilesData";
-import { useNavigate } from "react-router-dom";
+import { jobTilesData } from "../../../constants/tilesData";
+// import { useNavigate } from "react-router-dom";
 
 const AdminPanel = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  const handleTileClick = (tile) => {
-    if (tile.route) {
-      navigate(tile.route);
-    }
-  };
+  // const handleTileClick = (tile) => {
+  //   if (tile.route) {
+  //     navigate(tile.route);
+  //   }
+  // };
 
   return (
     <div>
@@ -22,14 +22,14 @@ const AdminPanel = () => {
       </div>
 
       {/* Render Ova2-Etoken Main Tile */}
-      <div className="tile-container">
+      {/* <div className="tile-container">
         {ova2EtokenMainTile.map((tile, index) => (
           <div key={index} className="tile" onClick={() => handleTileClick(tile)}>
             <h3>{tile.title}</h3>
             <p>{tile.description}</p>
             </div>
         ))}
-      </div>
+      </div> */}
     </div>
   );
 };
